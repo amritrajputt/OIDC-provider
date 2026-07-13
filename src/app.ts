dc1e2b9b02edd3b5178d9a02504a05a4ead49553
amrit <vishal179singh@gmail.com>
2026-07-13 22:30:47 +0530

    added new url in cors

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ const app = express();
 
 const allowedOrigins = [
     process.env.FRONTEND_URL || "http://localhost:5173",
-    "http://localhost:5174"
+    "http://localhost:5175"
 ];
 
 app.use(cors({
